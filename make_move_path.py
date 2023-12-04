577907e8b3f6fecb6ac069074936256e09a83a24
nranabhat <78765060+nranabhat@users.noreply.github.com>
2023-12-03 21:42:18 -0600

    adjusting origin and orientation

--- make_move_path.py
+++ make_move_path.py
@@ -1,3 +1,17 @@
+"""
+Chess Robot Movement Path Generator
+
+This script calculates the movement path for a robotic arm designed to move chess pieces on a chessboard. The script reads configuration details such as the board's dimensions and piece height from a YAML file and uses these to generate a 3D path that the robot will follow to move a chess piece from one square to another.
+
+The board is oriented such that the A1 square has the largest y-value and the smallest x-value, while the H8 square has the smallest y-value and the largest x-value. The script takes into account the height of the pieces, ensuring the robot arm moves just below the top of the piece for picking and dropping.
+
+Usage:
+Run the script and input the starting and ending chess square coordinates when prompted. The script will output a numpy array representing the robot's movement path.
+
+Author: nranabhat
+Date: 12/3/23
+"""
+
 import numpy as np
 import yaml
 
@@ -9,46 +23,41 @@ board_width = config['board']['width']
 board_height = config['board']['height']
 origin = np.array([config['origin']['x'], config['origin']['y'], config['origin']['z']])
 piece_height = config['piece']['height']
+# offset x 
+x_offset = 0.01
 
 # Function to convert chess square to coordinates
 def square_to_coord(square):
     col = ord(square[0].upper()) - ord('A')
-    row = 8 - int(square[1])
+    row = int(square[1]) - 1
     square_width = board_width / 8
     square_height = board_height / 8
-    x = (col * square_width) + (square_width / 2)
-    y = (row * square_height) + (square_height / 2)
+
+    x = x_offset + (row * square_height) + (square_height / 2)
+    y = (board_width / 2) - (col * square_width) - (square_width / 2)
     return np.array([x, y, 0])
 
-# Function to create the movement path
 def make_move_path(start, end):
     start_coord = square_to_coord(start)
     end_coord = square_to_coord(end)
 
     # Adjusting the height to be 1cm below/above the top of the piece
-    grip_surface_height = 0.01
-    # have to robot transfer pieces so that it doesn't knock over other pieces.
+    delta_h = 0.01
     
     # Path points
-    above_start = start_coord + np.array([0, 0, piece_height+grip_surface_height])
-    grip_start = above_start - np.array([0, 0, 2*grip_surface_height])
-    begin_transfer = above_start + np.array([0, 0, piece_height])
-    end_transfer = end_coord + np.array([0, 0, piece_height])
-    grip_end = end_coord - np.array([0, 0, piece_height+2*grip_surface_height])
-    above_end = grip_end + np.array([0, 0, 2*grip_surface_height])
+    path = np.array([
+        origin,                                          # Start at origin
+        start_coord + np.array([0, 0, piece_height + delta_h]),   # Move above the piece
+        start_coord + np.array([0, 0, piece_height - delta_h]),   # Lower to the piece
+        start_coord + np.array([0, 0, 2 * piece_height + delta_h]), # Lift the piece above the other ones
+        end_coord + np.array([0, 0, 2 * piece_height + delta_h]),   # Move to above the end square
+        end_coord + np.array([0, 0, piece_height - delta_h]),       # Lower the piece to end square
+        end_coord + np.array([0, 0, piece_height + delta_h]),       # Lift up from the end square
+        origin                                                      # Return to origin
+    ])
 
-    path = [
-        origin,                   # Start at origin
-        above_start,              # Move above the piece
-        grip_start,               # Lower to the piece
-        begin_transfer,           # Lift the piece above the other ones
-        above_end,                # Move to above the end square
-        end_transfer,             # Lower the piece to end square
-        above_end,                # Lift up from the end square
-        origin                    # Return to origin
-    ]
+    return path
 
-    return np.array(path)
 
 # Main execution
 if __name__ == "__main__":
